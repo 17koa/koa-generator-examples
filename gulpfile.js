d1261ac778c7cc611165e95959bd7005ebd546d4
i5ting <i5ting@126.com>
2015-12-13 13:50:06 +0800

    Update 2015-12-13T05:50:00.885Z

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,10 @@ var gulp = require('gulp');
 var ghPages = require('gulp-gh-pages');
 
 gulp.task('deploy', function() {
-  return gulp.src('./dist/**/*')
+  return gulp.src('./_book/**/*')
     .pipe(ghPages());
+});
+
+gulp.task('default', ['deploy'], function() {
+  console.log('complete!')
 });
